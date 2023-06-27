1390e631431752b35632cbd57f06955017e3d1be
Xaver Gruber <xaver.max.gruber@gmail.com>
2023-06-27 18:00:16 +0200

    add paypal submodule, add npm install in paypal folder for shopwareDeploy

--- shopwareDeploy.sh
+++ shopwareDeploy.sh
@@ -1,4 +1,7 @@
 cp ./backup/storefront.yaml ./config/packages/ 
+echo install paypal dependencies...
+cd custom/plugins/SwagPayPal/src/Resources/app/storefront && npm install
+cd ../../../../../../..
 echo vendor/deployer/deployer/dep deploy -vvv
 vendor/deployer/deployer/dep deploy -vvv
 rm ./config/packages/storefront.yaml
