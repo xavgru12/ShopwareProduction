9aef477ee6784b1912c33c18284e7d4a1356d62d
Xaver Gruber <xaver.max.gruber@gmail.com>
2023-06-27 18:31:59 +0200

    add system install to install script

--- installShopware.sh
+++ installShopware.sh
@@ -1,6 +1,9 @@
 echo composer install
 composer install
 
+echo install Shopware
+bin/console system:install --basic-setup --create-database
+
 echo bin/console plugin:install --activate SwagPayPal
 bin/console plugin:install --activate SwagPayPal
 
