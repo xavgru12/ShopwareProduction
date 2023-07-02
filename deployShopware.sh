f1c457381b80faece8355b2f44ed2bb27fb11331
Xaver Gruber <xaver.max.gruber@gmail.com>
2023-07-02 19:46:17 +0200

    add info text

--- deployShopware.sh
+++ deployShopware.sh
@@ -1,3 +1,4 @@
+#at first run the shared folder and database needs to be configured. Copy contents into shared folder and run bin/console --basic setup --create-database on webhost
 cp ./backup/storefront.yaml ./config/packages/ 
 echo install paypal dependencies...
 cd custom/plugins/SwagPayPal/src/Resources/app/storefront && npm install
